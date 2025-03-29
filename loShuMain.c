5d1ca65f15339ab0805d3e459f7a9d71dad8a4ad
Nicholas <nsbarkero@cpp.edu>
2025-03-28 22:14:56 -0700

    Created printing method

--- loShuMain.c
+++ loShuMain.c
@@ -10,20 +10,52 @@ static bool isMagicSquare(int array[3][3])
     // Calculate the sums of rows, columns, and diagonals
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            sums[i] += array[i][j]; // Calculate row sums
-            sums[j + 3] += array[i][j]; // Calculate column sums
-            if (i == j) sums[2 * 3] += array[i][j]; // Calculate main diagonal sum
-            if (i + j == 3 - 1) sums[2 * 3 + 1] += array[i][j]; // Calculate secondary diagonal sum
+            sums[i] += array[i][j]; // Calculate sums of row
+            sums[j + 3] += array[i][j]; // Calculate sums of column
+            if (i == j) sums[2 * 3] += array[i][j]; // Calculate first diagonal sums
+            if (i + j == 3 - 1) sums[2 * 3 + 1] += array[i][j]; // Calculate second diagonal sums
         }
     }
 
-    // Check if all sums are equal to the first row sum (index 0)
+    // Checks if all sums are equal
     for (int k = 1; k < 3 * 2 + 2; k++) {
         if (sums[k] != sums[0]) {
-            return false; // If any sum is not equal, it's not a Lo Shu Magic Square
+            return false; // If any sum is not, return false
         }
     }
 
     return true;
+}
+
+void printArray(int array[3][3]) //printing function for multi-dimensional array
+{
+    if(isMagicSquare(array) == true) //checks if it even should print
+    {
+        for (int i = 0; i < 3; i++) { //loops through the rows
+            for (int j = 0; j < 3; j++) //loops through the columns
+            {
+                //for formating
+                if(j == 0)
+                    {
+                        printf("["); 
+                        printf("%d ", array[i][j]);
+                    }
+                else if(j == 1)
+                    {
+                        printf("%d ", array[i][j]);
+                    }         
+                else if(j == 2)
+                    {
+                        printf("%d", array[i][j]);
+                        printf("]");
+                    }              
+            }
+            printf("\n");
+        }
+    }
+    else if(isMagicSquare(array) == false) //if false, then it is not a magic square
+    {
+        printf("This array is not a Lo Shu Magic Square\n\n");
+    }
 }
 
